fb1401d08fc2716c731309a7ac348e357d6e1f7f
Michael Haberler <git@mah.priv.at>
2023-06-01 13:26:28 +0200

    optionally suppress warning

--- src/SdFat.h
+++ src/SdFat.h
@@ -445,7 +445,9 @@ typedef FsBaseFile SdBaseFile;
 #if defined __has_include
 #if __has_include(<FS.h>)
 #define HAS_INCLUDE_FS_H
+#ifndef SUPPRESS_FS_H_WARNING 
 #warning File not defined because __has_include(FS.h)
+#endif
 #endif  // __has_include(<FS.h>)
 #endif  // defined __has_include
 #ifndef HAS_INCLUDE_FS_H
